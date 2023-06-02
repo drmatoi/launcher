21b7d9afbf65064da9d4330f225ea27f0fa31063
M4T01 <128631144+drmatoi@users.noreply.github.com>
2023-06-02 21:07:37 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -14,14 +14,14 @@ echo -e "$Red Welcome to Dr.Matoi´s Collection. \033[1;31m"
 sleep 5.0
 clear
 echo -e "$Cyan 
-╔══════════════════════════════════════════════════════════╦════════════════════╦═══════════════════════════════════════════╗
-║     Visti drmatoi.github.io  - t.me/moddingtool          ║   By Mr.Matoi      ║    ╭――――――――――――――――――――――――――――――――――――╮ ║  
-║══════════════════════════════════════════════════════════╬════════════════════╣    │   Everything I've ever worked on,  │ ║
-║     Acces to a big collection of Bash, Shell and         ║     ▬▬ι═══════ﺤ    ║    │  and other content like link lists │ ║
-║                   phyton scripts!                        ║                    ║    │       can be found here!           │ ║
-║         Please report not working links in "Issues"      ╠════════════════════╣    ╰――――――――――――――――――――――――――――――――――――╯ ║                 
-║                  github.com/drmatoi/matoi                ║     Version 1.0    ║                                           ║
-╚══════════════════════════════════════════════════════════╩════════════════════╩═══════════════════════════════════════════╝
+╔══════════════════════════════════════════════════════════╦════════════════════╗
+║     Visti drmatoi.github.io  - t.me/moddingtool          ║   By Mr.Matoi      ║  
+║══════════════════════════════════════════════════════════╬════════════════════╣
+║     Acces to a big collection of Bash, Shell and         ║     ▬▬ι═══════ﺤ    ║
+║                   phyton scripts!                        ║                    ║
+║         Please report not working links in "Issues"      ╠════════════════════╣               
+║                  github.com/drmatoi/matoi                ║     Version 1.0    ║
+╚══════════════════════════════════════════════════════════╩════════════════════╩
 
 
 \033[1;36m"
